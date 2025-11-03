9b6b049a6e30f131982fe42329d2dae8687768a1
EstefanoGiusiano <estefanogiusiano2607@gmail.com>
2025-11-03 20:50:46 -0300

    estilo en lista

--- FoodApp/Componentes/listaproducto.jsx
+++ FoodApp/Componentes/listaproducto.jsx
@@ -1,10 +1,37 @@
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import Producto from "./producto";
-import { View } from 'react-native';
 
 export default function ListaProductos({ productos, addToCarrito }) {
-    return (
-        <View className="productos">
-            {productos.map((p) => (<Producto key={p.id} producto={p} addToCarrito={addToCarrito} />))}
-        </View>
-    );
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.titulo}>🍴 Menú de Productos</Text>
+
+      <View style={styles.grid}>
+        {productos.map((p) => (
+          <Producto key={p.id} producto={p} addToCarrito={addToCarrito} />
+        ))}
+      </View>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: "#f2f2f2", 
+    padding: 16,
+    paddingBottom: 40,
+  },
+  titulo: {
+    fontSize: 26,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+    color: "#222",
+  },
+  grid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+});
