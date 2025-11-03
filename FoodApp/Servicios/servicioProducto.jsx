b0e46ec57d43963b4960fcae7910447d4471c267
FranciscoAlonsoE <francisco.alonsoe@correo.ucu.edu.uy>
2025-11-03 20:33:48 -0300

    change servicio to get Json

--- FoodApp/Servicios/servicioProducto.jsx
+++ FoodApp/Servicios/servicioProducto.jsx
@@ -1,33 +1,20 @@
-const API_URL = "http://192.168.0.105:3001/productos";
+import productsData from "../data/products.json";
 
 export const getProductos = async () => {
-    try {
-        const respuesta = await fetch(API_URL);
-        if (!respuesta.ok) {
-            throw new Error("Error al obtener los productos");
-        }
-        return await respuesta.json();
-    } catch (error) {
-        console.error("Error en getProductos:", error);
-        throw error;
-    }
+  try {
+    return productsData.products;
+  } catch (error) {
+    console.error("Error en getProductos:", error);
+    throw error;
+  }
 };
 
 export const addProducto = async (producto) => {
-    try {
-        const respuesta = await fetch(API_URL, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(producto),
-        });
-
-        if (!respuesta.ok) {
-            throw new Error("Error al agregar el producto");
-        }
-
-        return await respuesta.json();
-    } catch (error) {
-        console.error("Error en addProducto:", error);
-        throw error;
-    }
+  try {
+    productsData.products.push(producto);
+    return producto;
+  } catch (error) {
+    console.error("Error en addProducto:", error);
+    throw error;
+  }
 };
