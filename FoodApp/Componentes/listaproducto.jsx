b624e3bfaac3f8f7372c20c7a8034f2c03d228c8
Juan Ignacio Weyrauch <juan.weyrauch@correo.ucu.edu.uy>
2025-11-04 13:04:30 -0300

    changed tabs for stacks, made some css mods (#20)
    - Changed the design of the app to use Stacks isntead of Tabs.
    - Changed some CSS design flaws.
    - Added the web dev kit (installed idk if it stays)

--- FoodApp/Componentes/listaproducto.jsx
+++ FoodApp/Componentes/listaproducto.jsx
@@ -1,10 +1,25 @@
 import Producto from "./producto";
-import { View } from 'react-native';
+import { View, StyleSheet } from "react-native";
 
 export default function ListaProductos({ productos, addToCarrito }) {
-    return (
-        <View className="productos">
-            {productos.map((p) => (<Producto key={p.id} producto={p} addToCarrito={addToCarrito} />))}
-        </View>
-    );
+  return (
+    <View style={styles.ListaProductos}>
+      {productos.map((p) => (
+        <Producto
+          key={p.id}
+          producto={p}
+          addToCarrito={addToCarrito}
+        />
+      ))}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  ListaProductos: {
+    flex: 1,
+    alignItems: "stretch",
+    justifyContent: "center",
+    paddingHorizontal: 0,
+  },
+});
