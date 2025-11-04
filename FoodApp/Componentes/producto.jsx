9284eeec3c1ece2bdf8dc705b56a0775caa96ff1
Juan Ignacio Weyrauch <juan.weyrauch@correo.ucu.edu.uy>
2025-11-04 13:26:49 -0300

    changed the styles to make it grid and delete the 'estiloProductos' branch bc of all the errors it had.

--- FoodApp/Componentes/producto.jsx
+++ FoodApp/Componentes/producto.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function Producto({ producto, addToCarrito }) {
@@ -9,45 +10,69 @@ export default function Producto({ producto, addToCarrito }) {
       onPress={() => !sinStock && addToCarrito(producto.id)}
       disabled={sinStock}
     >
-      <View style={styles.infoContainer}>
-        <Text style={styles.nombre}>{producto.name}</Text>
-        <Text style={styles.stock}>
-          {sinStock ? "No stock" : `Stock: ${producto.stock}`}
-        </Text>
-        <Text style={styles.precio}>💲{producto.price}</Text>
+      <View style={styles.header}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={styles.emoji}>{producto.emoji}</Text>
+          <Text style={styles.nombre} numberOfLines={2} adjustsFontSizeToFit>
+            {producto.name}
+          </Text>
+        </View>
       </View>
+
+      <Text style={styles.stock}>
+        {sinStock ? "No stock" : `Stock: ${producto.stock}`}
+      </Text>
+      <Text style={styles.precio}>💲{producto.price}</Text>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   producto: {
-    backgroundColor: "#b0aeaeff",
+    backgroundColor: "#fffaf0",
     padding: 16,
-    marginVertical: 8,
+    margin: 8,
     borderRadius: 10,
+    alignItems: "center",
+    elevation: 3,
+    width: "45%", // Takes up slightly less than half to account for margins
+    aspectRatio: 1, // Makes it a perfect square
   },
   disabled: {
     opacity: 0.4,
   },
-  infoContainer: {
-    alignItems: "center", 
-    justifyContent: "center", 
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    marginBottom: 8,
+    flexWrap: "wrap",
+  },
+  emoji: {
+    fontSize: 24,
+    marginRight: 4,
   },
   nombre: {
-    fontSize: 22,
+    fontSize: 18,
     fontWeight: "bold",
+    flexShrink: 1,
     textAlign: "center",
+    flexWrap: "wrap",
   },
   stock: {
     fontSize: 16,
     color: "#555",
-    textAlign: "center",
   },
   precio: {
     fontSize: 18,
     color: "green",
     marginTop: 4,
-    textAlign: "center",
   },
 });
