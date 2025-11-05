94856bfde4b717fe2cd0ea77e8de328f36df4d14
Juan Ignacio Weyrauch <juan.weyrauch@correo.ucu.edu.uy>
2025-11-05 20:26:56 -0300

    Changed logic from App.jsx to HomeScreen.jsx (#37)
    * changed logic from app to homeScreen

--- FoodApp/cambios/App.js
+++ FoodApp/cambios/App.js
@@ -1,14 +1,41 @@
 import { StyleSheet, TouchableOpacity, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { CartProvider } from "./context/cartContext";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import HomeScreen from "./Componentes/Screens/HomeScreen.jsx";
 import CartScreen from "./Componentes/Screens/CartScreen.jsx";
+import { CartProvider } from "./context/cartContext";
+import ListaProductos from "./Componentes/listaproducto";
 import Landing from "./Componentes/Screens/LandingScreen.jsx";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
 const PRIMARY = "#1E88E5";
+
+
+function ProductosTabs() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Comidas">
+        {() => <ListaProductos categoria="comida" />}
+      </Tab.Screen>
+      <Tab.Screen name="Meriendas">
+        {() => <ListaProductos categoria="meriendas" />}
+      </Tab.Screen>
+      <Tab.Screen name="Bebidas">
+        {() => <ListaProductos categoria="bebidas" />}
+      </Tab.Screen>
+      <Tab.Screen name="Postres">
+        {() => <ListaProductos categoria="postres" />}
+      </Tab.Screen>
+      <Tab.Screen name="Todos">
+        {() => <ListaProductos />}
+      </Tab.Screen>
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -20,9 +47,11 @@ export default function App() {
             component={Landing}
             options={{ headerShown: false }}
           />
+
           <Stack.Screen
             name="Home"
-            component={HomeScreen}
+            // component={HomeScreen}
+            component={ProductosTabs}
             options={({ navigation }) => ({
               title: "FoodApp",
               headerStyle: { backgroundColor: PRIMARY },
@@ -39,6 +68,7 @@ export default function App() {
               ),
             })}
           />
+
           <Stack.Screen
             name="Cart"
             component={CartScreen}
